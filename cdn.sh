fb6e74dce0eded57f0b53547d9ac0c9e7f4247af
= <=>
2023-02-23 08:07:20 +0800

    去除无用依赖

--- cdn.sh
+++
@@ -1,6 +0,0 @@
-npx esbuild src/cdn.ts \
-  --bundle \
-  --outfile=lib/cdn.js \
-  --sourcemap \
-  --format=esm \
-  --watch
--- dev.sh
+++
@@ -1,7 +0,0 @@
-npx esbuild src/core/index.ts \
-  --bundle \
-  --outfile=lib/index.js \
-  --sourcemap \
-  --format=esm \
-  --watch \
-  --minify
